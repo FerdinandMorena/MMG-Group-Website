a0d63f9b38dd2173de560ff6d8689f64b0a4a828
Ferdinand <fmmphahle01@gmail.com>
2026-04-10 09:53:48 +0200

    pb

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -75,7 +75,7 @@ export default function Home() {
         />
         <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/40 to-black/70" />
 
-        <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20">
+        <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20 pb-20">
           <motion.div
             className="max-w-3xl"
             variants={containerVariants}
